4d91fe431f9992b005ff5799c068629cc586d6d7
Gabriel Evans <gabriel@codeconcoction.com>
2013-02-17 15:40:58 -0800

    Add Contraband .import, .import_async, and .logger

--- lib/contraband.rb
+++ lib/contraband.rb
@@ -11,4 +11,57 @@ require 'contraband/railtie' if defined?(Rails)
 I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales', 'en.yml')
 
 module Contraband
+  extend self
+
+  ##
+  # @example
+  #     Contraband.import(Band, :lastfm, resource)
+  #
+  # @param [Class] model
+  #   The model to import.
+  #
+  # @param [Symbol] service
+  #   The service of the resource.
+  #
+  # @param [Object] resource
+  #   The resource to import.
+  #
+  def import(model, service, resource)
+    model.importer_class(service).import(resource)
+  end
+
+  ##
+  # @param [Class] model
+  #   The model to import.
+  #
+  # @param [Symbol] service
+  #   The service of the resource.
+  #
+  # @param [String] id
+  #   The identifier of the resource.
+  #
+  # @param [Hash] data
+  #   The resource data to be imported.
+  #
+  def import_async(model, service, id, data = {})
+    defined?(DeferredImport) or raise Errors::DeferredImporterMissing
+    DeferredImport.import(model.importer_class(service), id, data)
+  end
+
+  def logger
+    @logger ||= (rails_logger || default_logger)
+  end
+
+  private
+
+  def rails_logger
+    defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
+  end
+
+  def default_logger
+    require 'logger'
+    logger = Logger.new($stdout)
+    logger.level = Logger::INFO
+    logger
+  end
 end # Contraband
--- spec/contraband_spec.rb
+++ spec/contraband_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Contraband do
-  pending
+
+  describe '.import' do
+
+    it 'imports the provided resource through the model importer' do
+      importer = double(Contraband::Importer)
+      importer.should_receive(:import).with(id: '123', message: 'like dis if u cry evertiem')
+      Status.should_receive(:importer_class).with(:facebook).and_return(importer)
+      Contraband.import(Status, :facebook, id: '123', message: 'like dis if u cry evertiem')
+    end
+  end
+
+  describe '.import_async' do
+
+    context 'when DeferredImport is not defined' do
+
+      it 'raises Errors::DeferredImporterMissing' do
+        expect {
+          Contraband.import_async(Status, :facebook, '123')
+        }.to raise_error(Contraband::Errors::DeferredImporterMissing)
+      end
+    end
+
+    context 'when DeferrredImport is defined' do
+
+      it 'calls DeferredImport.import with model, service, id, and data' do
+        deferred_importer = double('DeferredImport')
+        deferred_importer.should_receive(:import).with(TwitterImporter::Status, '123', { foo: 'bar', baz: 'bloop' })
+        stub_const('DeferredImport', deferred_importer)
+        Contraband.import_async(Status, :twitter, '123', { foo: 'bar', baz: 'bloop' })
+      end
+    end
+  end
 end
